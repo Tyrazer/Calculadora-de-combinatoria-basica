0659e85dfdd038bb5e54ee355a66db6917370a1f
Tyrazer <tonybattikahoche@gmail.com>
2026-04-02 13:39:40 -0400

    Calculadora de combinaciones y de numero de Stirling de 2da clase

--- interfaz.py
+++ interfaz.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 from tkinter import messagebox
 from Operaciones import combinacion
+from Operaciones import stirling_second
 
 def calcular():
     try:
         n=int(n_entry.get())
         k=int(k_entry.get()) 
-        resultado=combinacion(n,k)
-        resultado_label["text"]=resultado
+        if var_op.get()==1:
+            resultado=combinacion(n,k)
+            resultado_label.config(text="Resultado: " + str(resultado))
+        elif var_op.get()==2:
+            resultado=stirling_second(n,k)
+            resultado_label.config(text="Resultado: " + str(resultado))
     except ValueError:
         messagebox.showerror("Error", "Ingrese números enteros válidos")
     except RecursionError:
@@ -40,6 +45,13 @@ k_label.grid(row=0,column=2)
 k_entry=tk.Entry(frame_entradas, width=10)
 k_entry.grid(row=0,column=3)
 
+#Selección de operación
+var_op=tk.IntVar(value=1)
+rb_comb=tk.Radiobutton(ventana,text="Combinación",variable=var_op, value=1)
+rb_stirling=tk.Radiobutton(ventana,text="Stirling 2da clase", variable=var_op, value=2)
+rb_comb.pack()
+rb_stirling.pack()
+
 #Boton de calcular
 boton_calc=tk.Button(ventana,text="CALCULAR",bg="blue",fg="white",font=("Arial", 12, "bold"), command=calcular)
 boton_calc.pack(pady=15)
@@ -47,6 +59,7 @@ boton_calc.pack(pady=15)
 #Etiqueta para mostrar resultado
 resultado_label=tk.Label(ventana,text="Resultado:", fg="Green", font=("Arial", 12, "bold"))
 resultado_label.pack(side="bottom")
+
 
 #Mantiene la ventana abierta
 ventana.mainloop()
